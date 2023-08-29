e5fd9f0ad57be9f8e1b0f24d24af4b5795ecfb15
Avinash Chakka <chakkaavinash123@gmail.com>
2023-08-29 13:22:48 +0530

    My First Commit

--- src/main/java/com/amigoscode/movie/MovieDataAccessService.java
+++ src/main/java/com/amigoscode/movie/MovieDataAccessService.java
@@ -9,25 +9,57 @@ import java.util.Optional;
 @Repository
 public class MovieDataAccessService implements MovieDao {
 
+    private final JdbcTemplate jdbcTemplate;
+    public MovieDataAccessService(JdbcTemplate jdbcTemplate)
+    {
+        this.jdbcTemplate=jdbcTemplate;
+    }
+
+
     @Override
     public List<Movie> selectMovies() {
-        throw new UnsupportedOperationException("not implemented");
+        var sql= """ 
+                   SELECT id,name,release_date 
+                   FROM movie
+                   limit 100; 
+                    """;
+        return jdbcTemplate.query(sql,new MovieRoleMapper());
     }
 
     @Override
     public int insertMovie(Movie movie) {
-        throw new UnsupportedOperationException("not implemented");
+        String sql= """ 
+                    INSERT INTO movie(name,release_date) 
+                    VALUES (?,?);
+                    """;
+        return jdbcTemplate.update(
+                sql,
+                movie.name(),movie.releaseDate()
+        );
+
     }
 
     @Override
     public int deleteMovie(int id) {
-        throw new UnsupportedOperationException("not implemented");
-
+        var sql= """ 
+                   DELETE FROM movie
+                   WHERE id=?; 
+                    """;
+        return jdbcTemplate.update(sql,id);
     }
 
     @Override
-    public Optional<Movie> selectMovieById(int id) {
-        throw new UnsupportedOperationException("not implemented");
+    public Optional<Movie> selectMovieById(int id)
+    {
+        var sql= """ 
+                   SELECT id,name,release_date 
+                   FROM movie
+                   WHERE id=?; 
+                    """;
+        return jdbcTemplate.
+                query(sql,new MovieRoleMapper(),id)
+                .stream()
+                .findFirst();
     }
     
 }
---
+++ src/main/java/com/amigoscode/movie/MovieRoleMapper.java
@@ -0,0 +1,22 @@
+package com.amigoscode.movie;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class MovieRoleMapper implements RowMapper<Movie>
+{
+
+    @Override
+    public Movie mapRow(ResultSet resultSet, int i) throws SQLException {
+        return new Movie(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                List.of(),
+                LocalDate.parse(resultSet.getString("release_date"))
+        );
+    }
+}
